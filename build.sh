9a147477724852a4db41f4391cc8998af44249c7
Erlimar Silva Campos <erlimar@gmail.com>
2019-11-06 16:36:01 -0300

    Add initial code

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -ou pipefail
+
+exists()
+{
+    if which $1 &>/dev/null; then
+        return 0;
+    else
+        return 1
+    fi
+}
+
+MAIN=./src/main.py
+
+if ! exists pyinstaller; then
+    echo "PyInstaller not installed!"
+    echo "Run:"
+    echo "  $ pip install -r requirements.txt"
+    exit 1
+fi
+
+pyinstaller --onefile --console -n deployer ${MAIN}
+
+if [ $? != 0 ]; then
+    echo "Build failed."
+    exit $?
+fi
+
+echo "To test, exec: ./dist/deployer"
+
